099dfa3f7114be17f3b28fb6db1cb06891926f0c
KG <kganza@traxiontech.net>
2021-11-02 11:03:04 +0800

    Initial Commit

---
+++ resources/js/Pages/Workout/Workouts.vue
@@ -0,0 +1,28 @@
+<template>
+    <Head title="Workouts"/>
+
+    <breeze-authenticated-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Workouts
+            </h2>
+        </template>
+        {{ workouts }}
+    </breeze-authenticated-layout>
+
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import { Head } from '@inertiajs/inertia-vue3';
+
+export default {
+    components: {
+        BreezeAuthenticatedLayout,
+        Head,
+    },
+    props: {
+        workouts: Number
+    }
+}
+</script>
